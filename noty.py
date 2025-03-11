779b38e4d71f1e6b7b729231238f102e64ffcf6a
unknown <kristyna.pavlova@infis.cz>
2025-03-11 08:40:01 +0100

    text

---
+++ noty.py
@@ -0,0 +1,13 @@
+#nacist text a prety povani a podminky aq logicke operatory a vypsani
+cislo = int(input("zadejte cislo"))
+if cislo<5:
+        print("cislo je vetsi než 5")
+elif cislo==6 and cislo==7:
+        print("cislo je 6nebo je cislo 7")
+else:
+        print("cislo je mensi nez 5")
+# git hub
+#git add *
+# git commit -m "text"
+# git push
+# ktistyna pavlova
